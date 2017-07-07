ad6b0758886557905702a0c991b34e77f4c37b86
feitian124 <feitian124@gmail.com>
2017-07-07 23:40:19 +0800

    read topic from cnodejs

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <router-link to="/topics">Go to Topics</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
@@ -16,6 +11,7 @@
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
   </div>
 </template>
@@ -25,7 +21,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome, ming'
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Topics from '@/components/Topics'
+import Bar from '@/components/Bar'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+    { path: '/', name: 'Hello', component: Hello },
+    { path: '/topics', component: Topics },
+    { path: '/bar', component: Bar }
   ]
 })
